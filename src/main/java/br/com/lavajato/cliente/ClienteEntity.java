7286d4dca62481e4f9300a5009c300ced894bf83
lanussehelena <lanusse.dev@gmail.com>
2026-04-25 20:11:06 -0300

    Update: Atualiza ClienteEntity

--- src/main/java/br/com/lavajato/cliente/ClienteEntity.java
+++ src/main/java/br/com/lavajato/cliente/ClienteEntity.java
@@ -1,9 +1,11 @@
 package br.com.lavajato.cliente;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
 @Table
+@Data
 
 public class ClienteEntity {
     @Id
@@ -12,6 +14,9 @@ public class ClienteEntity {
 
     @Column(nullable = false, length = 100)
     private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String email;
 
     public  ClienteEntity() {}
 }
