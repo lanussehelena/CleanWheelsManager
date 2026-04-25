595495b76257162bda50cd815f94a5b54acf0fe0
lanussehelena <lanusse.dev@gmail.com>
2026-04-25 18:59:24 -0300

    Configurações iniciais

---
+++ src/main/java/br/com/lavajato/CleanwheelsManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.lavajato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanwheelsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanwheelsManagerApplication.class, args);
+	}
+
+}
