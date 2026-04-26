c20abecb35113950b38a7d1a3d95607c5810812b
Luh Ramiro <ramiro.lucivania@gmail.com>
2026-04-26 10:16:57 -0300

    Adiciona VeiculoRepository

---
+++ src/main/java/br/com/lavajato/veiculo/VeiculoRepository.java
@@ -0,0 +1,8 @@
+package br.com.lavajato.veiculo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VeiculoRepository extends JpaRepository<VeiculoEntity, Long> {
+}
