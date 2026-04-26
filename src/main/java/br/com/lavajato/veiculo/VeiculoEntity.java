d3aaf5b98a446b4a690ba7485b02cf793a2f2194
Luh Ramiro <ramiro.lucivania@gmail.com>
2026-04-26 10:07:54 -0300

    Adiciona VeiculoEntity

---
+++ src/main/java/br/com/lavajato/veiculo/VeiculoEntity.java
@@ -0,0 +1,32 @@
+package br.com.lavajato.veiculo;
+
+import br.com.lavajato.cliente.ClienteEntity;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table
+@Data
+public class VeiculoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String marca;
+
+    @Column(nullable = false, length = 50)
+    private String modelo;
+
+    @Column(nullable = false, unique = true, length = 10)
+    private String placa;
+
+    @Column(length = 20)
+    private String cor;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private ClienteEntity cliente;
+
+    public VeiculoEntity() {}
+}
